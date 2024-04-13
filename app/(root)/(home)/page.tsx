c80378755b298180458737f27e5caf1e36a10040
Abderrahmane-belarbi <abdoustart007@gmail.com>
2024-04-13 15:14:18 +0100

    adding navbar logo clerk's userbutton colors

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/" />
-      <h1 className="h1-bold">This is a pice of text</h1>
+      
     </div>
   );
 }
