769f0d30030ccd783b0b3d6ae8a794c816a84ea1
Abderrahmane-belarbi <abdoustart007@gmail.com>
2024-04-19 16:13:51 +0100

    adding-api-webhook-route-and-creating-user-create-update-delete

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,9 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import Question from "@/database/question.model";
+import { CreateUserParams, DeleteUserParams, UpdateUserParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
 
 export async function getUserById(params:any) {
     try {
@@ -15,3 +18,55 @@ export async function getUserById(params:any) {
         throw error;
     }
 }
+
+
+export async function createUser(userData: CreateUserParams) {
+    try {
+      connectToDatabase();
+      const newUser = await User.create(userData);
+      return newUser;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+  
+  export async function updateUser(params: UpdateUserParams) {
+    try {
+      connectToDatabase();
+      const { clerkId, updateData, path } = params;
+      await User.findOneAndUpdate({ clerkId }, updateData, {
+        new: true,
+      });
+      revalidatePath(path);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+  
+  export async function deleteUser(params: DeleteUserParams) {
+    try {
+      connectToDatabase();
+      const { clerkId } = params;
+      const user = await User.findOneAndDelete({ clerkId });
+      if (!user) {
+        throw new Error("User not found");
+      }
+
+      // get user question ids
+      const userQuestionIds = await Question.find({author: user._id}).distinct('_id');
+
+      // Delete user questions
+      await Question.deleteMany({author: user._id});
+
+      // TODO: delete user answers, comments, etc
+ 
+      // Finally, delete the user
+      const deletedUser = await User.findByIdAndDelete(user._id);
+      return deletedUser;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
