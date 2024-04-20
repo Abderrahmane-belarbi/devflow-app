b3650aa305f68700ca776489980cc5393ded188d
Abderrahmane-belarbi <abdoustart007@gmail.com>
2024-04-20 09:19:30 +0100

    add api into app folder

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -63,6 +63,7 @@ export async function POST(req: Request) {
       email: email_addresses[0].email_address,
       picture: image_url,
     });
+    console.log('mongoUser: ', mongoUser);
     return NextResponse.json({ message: "OK", user: mongoUser });
   }
 
