dbb39a9d0ce1045530393341e1bcaec73c81ccec
Abderrahmane-belarbi <abdoustart007@gmail.com>
2024-04-22 17:39:56 +0100

    deploy

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,10 +1,11 @@
 import QuestionForm from "@/components/forms/QuestionForm";
 import { getUserById } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
-  const userId  = "123456";
-  //const {userId } = auth();
+  //const userId  = "123456";
+  const { userId } = auth();
 
   if(!userId) {
     redirect('/sign-in');
--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,22 @@
 import { authMiddleware } from "@clerk/nextjs";
-
+ 
 export default authMiddleware({
   publicRoutes: [
-    "/",
-    "/api/webhook",
-    "/ask-question",
-    "/question/:id",
-    "/tags",
-    "/tags/:id",
-    "/profile/:id",
-    "/community",
-    "/jobs",
+    '/',
+    '/api/webhook',
+    'question/:id',
+    '/tags',
+    '/tags/:id',
+    '/profile/:id',
+    '/community',
+    '/jobs'
   ],
   ignoredRoutes: [
-    "/api/webhook",
-    "/api/chatgpt",
-    "/ask-question",
-    "/((?!api|trpc))(_next.*|.+\.[\w]+$)"
-  ],
+    '/api/webhook', '/api/chatgpt'
+  ]
 });
-
+ 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+ 
