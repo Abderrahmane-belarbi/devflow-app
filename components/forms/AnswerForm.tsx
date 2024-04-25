7a54a1862bcc735b0a5536ca7a6697a70918a4e1
Abderrahmane-belarbi <abdoustart007@gmail.com>
2024-04-25 09:13:27 +0100

    creating a answer form component and adding adding schema answer to the validation.ts for the input

--- components/forms/AnswerForm.tsx
+++ components/forms/AnswerForm.tsx
@@ -1,3 +1,123 @@
+"use client";
+
+import { CreateAnswerParams } from "@/lib/actions/shared.types";
+import { AnswerSchema } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, useForm } from "react-hook-form";
+import { z } from "zod";
+import { FormField } from "../ui/form";
+import {
+  FormControl,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Editor } from "@tinymce/tinymce-react";
+import { useTheme } from "@/context/ThemeProvider";
+import { useRef, useState } from "react";
+import { Button } from "../ui/button";
+import Image from "next/image";
+
 export default function AnswerForm() {
-    return <div>Answer</div>
+  const form = useForm<z.infer<typeof AnswerSchema>>({
+    resolver: zodResolver(AnswerSchema),
+    defaultValues: {
+      answer: "",
+    },
+  });
+
+  function handleCreateAnswer(){
+
+  }
+
+  const editorRef = useRef(null);
+  const { mode } = useTheme();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  return (
+    <div>
+      <div className="flex flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2 mt-6">
+        <h4 className="paragraph-semibold">
+          Write your answer here
+        </h4>
+
+        <Button 
+          className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
+          onClick={()=>{}}
+        >
+          <Image
+            src="/assets/icons/stars.svg"
+            alt="ai"
+            width={12}
+            height={12}
+            className="object-contain"
+          />
+          Generate an AI Answer
+        </Button>
+      </div>
+    <Form {...form}>
+      <form
+        className="mt-6 w-full flex flex-col gap-10"
+        onSubmit={form.handleSubmit(handleCreateAnswer)}
+      >
+        <FormField
+          control={form.control}
+          name="answer"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col gap-3">
+              <FormControl className="mt-3.5">
+                {/* Question explination input */}
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                  // @ts-ignore
+                  onInit={(evt, editor) => (editorRef.current = editor)}
+                  initialValue=""
+                  onBlur={field.onBlur}
+                  onEditorChange={(content) => field.onChange(content)}
+                  init={{
+                    height: 350,
+                    menubar: false,
+                    plugins: [
+                      "advlist",
+                      "autolink",
+                      "lists",
+                      "link",
+                      "image",
+                      "charmap",
+                      "preview",
+                      "anchor",
+                      "searchreplace",
+                      "visualblocks",
+                      "codesample",
+                      "fullscreen",
+                      "insertdatetime",
+                      "media",
+                      "table",
+                    ],
+                    toolbar:
+                      "undo redo | codesample | " +
+                      "bold italic forecolor | alignleft aligncenter " +
+                      "alignright alignjustify | bullist numlist",
+                    content_style: "body { font-family:Inter; font-size:16px }",
+                    skin: mode === "dark" ? "oxide-dark" : "oxide",
+                    content_css: mode === "dark" ? "dark" : "light",
+                  }}
+                />
+              </FormControl>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+        <div className="flex justify-end">
+          <Button
+            type="button"
+            className="primary-gradient w-fit text-white"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'Submitting...' : 'Submit'}
+          </Button>
+        </div>
+      </form>
+    </Form>
+    </div>
+  );
 }
--- lib/validation.ts
+++ lib/validation.ts
@@ -5,3 +5,7 @@ export const questionSchema = z.object({
   explanation: z.string().min(10).max(1000),
   tags: z.array(z.string().min(1).max(13)).min(1).max(3),
 });
+
+export const AnswerSchema = z.object({
+  answer:z.string().min(10)
+})
