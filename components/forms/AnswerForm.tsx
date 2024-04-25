14e91c1b20307815e64cd8af6404e617c8e6f1d7
Abderrahmane-belarbi <abdoustart007@gmail.com>
2024-04-25 09:59:08 +0100

    delete answer form

--- components/forms/AnswerForm.tsx
+++ components/forms/AnswerForm.tsx
@@ -53,70 +53,7 @@ export default function AnswerForm() {
           Generate an AI Answer
         </Button>
       </div>
-    <Form {...form}>
-      <form
-        className="mt-6 w-full flex flex-col gap-10"
-        //onSubmit={form.handleSubmit(handleCreateAnswer)}
-      >
-        <FormField
-          control={form.control}
-          name="answer"
-          render={({ field }) => (
-            <FormItem className="flex w-full flex-col gap-3">
-              <FormControl className="mt-3.5">
-                {/* Question explination input */}
-                <Editor
-                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
-                  // @ts-ignore
-                  onInit={(evt, editor) => (editorRef.current = editor)}
-                  initialValue=""
-                  onBlur={field.onBlur}
-                  onEditorChange={(content) => field.onChange(content)}
-                  init={{
-                    height: 350,
-                    menubar: false,
-                    plugins: [
-                      "advlist",
-                      "autolink",
-                      "lists",
-                      "link",
-                      "image",
-                      "charmap",
-                      "preview",
-                      "anchor",
-                      "searchreplace",
-                      "visualblocks",
-                      "codesample",
-                      "fullscreen",
-                      "insertdatetime",
-                      "media",
-                      "table",
-                    ],
-                    toolbar:
-                      "undo redo | codesample | " +
-                      "bold italic forecolor | alignleft aligncenter " +
-                      "alignright alignjustify | bullist numlist",
-                    content_style: "body { font-family:Inter; font-size:16px }",
-                    skin: mode === "dark" ? "oxide-dark" : "oxide",
-                    content_css: mode === "dark" ? "dark" : "light",
-                  }}
-                />
-              </FormControl>
-              <FormMessage className="text-red-500" />
-            </FormItem>
-          )}
-        />
-        <div className="flex justify-end">
-          <Button
-            type="button"
-            className="primary-gradient w-fit text-white"
-            disabled={isSubmitting}
-          >
-            {isSubmitting ? 'Submitting...' : 'Submit'}
-          </Button>
-        </div>
-      </form>
-    </Form>
+    
     </div>
   );
 }
