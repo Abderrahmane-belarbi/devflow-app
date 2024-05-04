3a38d3be87f4569a612a944b4d8d7d6ba1da96d9
Abderrahmane-belarbi <abdoustart007@gmail.com>
2024-05-04 12:09:50 +0100

    add jobs page is under development

---
+++ app/(root)/jobs/page.tsx
@@ -0,0 +1,19 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center mt-20 h-screen">
+      <h1 className="text-6xl font-bold mb-4 text-primary-500">404</h1>
+
+      <h3 className="h3-bold text-dark200_light900">
+        The jobs page is under development. 🛠️
+      </h3>
+      <div className="max-w-[400px]">
+      <p className="text-md text-dark200_light900 text-center mt-10">
+        We apologize for any inconvenience this may have caused. Our team is
+        actively working on bringing this page to you. <br/> Rest assured, the development process won't take long, and we appreciate
+        your patience. <br/> In the meantime, feel free to explore our other offerings.
+      </p>
+
+      </div>
+    </div>
+  );
+}
